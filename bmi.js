ecdec92a7f690d8d4c1449af533bd2180d9a3055
krishnagithub03 <agrawalkrishna030@gmail.com>
2022-11-15 03:58:38 +0530

    Final Commit

--- bmi.js
+++ bmi.js
@@ -22,13 +22,28 @@ button.addEventListener('click', () => {
 
     if(height_status && weight_status){
         const bmi = (weight / ((height*height)/10000)).toFixed(2);
-
         if(bmi < 18.6){
             result.innerHTML = 'Under weight : ' + bmi;
+            var mydiv = document.getElementById("output");
+            var aTag = document.createElement('a');
+            aTag.setAttribute('href',"Underweight.html");
+            aTag.innerText = "link text";
+            mydiv.appendChild(aTag);
+
         }else if(bmi >= 18.6 && bmi < 24.9){
             result.innerHTML = 'Normal : ' + bmi;
+            var mydiv = document.getElementById("output");
+            var aTag = document.createElement('a');
+            aTag.setAttribute('href',"Normal.html");
+            aTag.innerText = "link text";
+            mydiv.appendChild(aTag);
         }else{
             result.innerHTML = 'Over weight : ' + bmi;
+            var mydiv = document.getElementById("output");
+            var aTag = document.createElement('a');
+            aTag.setAttribute('href',"Overweight.html");
+            aTag.innerText = "link text";
+            mydiv.appendChild(aTag);
         }
     }else{
         alert('Invalid input');
